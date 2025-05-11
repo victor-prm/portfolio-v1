4cde2d7b4c24201fa0f64f6c44252b18668df853
victor-prm <victorpabb@gmail.com>
2025-05-11 22:48:59 +0200

    added some project data

---
+++ src/js/projects.js
@@ -0,0 +1,84 @@
+export function allProjects() {
+    return [
+        {
+            "title": "Pour Reception",
+            "year": "2017",
+            "roles": "Interaction Designer",
+            "tags": "Arduino, Sound Design, Capacitance, AI",
+            "type": "School Work, Exhibition",
+            "num_images": "7"
+        },
+        {
+            "title": "HAVE",
+            "year": "2018",
+            "roles": "Interaction Designer",
+            "tags": "Arduino, Light Design, Plants",
+            "type": "School Work",
+            "num_images": "9"
+        },
+        {
+            "title": "Elmer",
+            "year": "2017",
+            "roles": "Interaction Designer",
+            "tags": "Arduino, Raspberry Pi, Slow Technology, NFC/RFID",
+            "type": "School Work",
+            "num_images": "3"
+        },
+        {
+            "title": "Newsify",
+            "year": "2025",
+            "roles": "Frontend Developer",
+            "tags": "HTML, Sass, Javascript, Vite, Web Components",
+            "type": "School Work",
+            "num_images": "6"
+        },
+        {
+            "title": "Feedelitist",
+            "year": "2023",
+            "roles": "Frontend Developer",
+            "tags": "React, Noodl, AI, MongoDB, HTML, Javascript, CSS, Figma",
+            "type": "Client Work",
+            "num_images": "9"
+        },
+        {
+            "title": "Noodl",
+            "year": "2022",
+            "roles": "Design Technologist, Frontend Developer",
+            "tags": "React, Noodl, AI, MongoDB, HTML, Javascript, CSS, Figma",
+            "type": "Client Work",
+            "num_images": "12"
+        },
+        {
+            "title": "Prodikt",
+            "year": "2023",
+            "roles": "Design Technologist, Frontend Developer",
+            "tags": "React, Noodl, Puppeteer",
+            "type": "Client Work",
+            "num_images": "4"
+        },
+        {
+            "title": "Shine",
+            "year": "2022",
+            "roles": "Frontend Developer",
+            "tags": "React, Noodl, AI, MongoDB, HTML, Javascript, CSS, Figma",
+            "type": "Client Work",
+            "num_images": "5"
+        },
+        {
+            "title": "Third Nature",
+            "year": "2023",
+            "roles": "Interaction Designer, Frontend Developer",
+            "tags": "React, Noodl, AI, MongoDB, HTML, Javascript, CSS, Figma",
+            "type": "Client Work",
+            "num_images": "5"
+        },
+        {
+            "title": "Bernhard",
+            "year": "2017",
+            "roles": "Interaction Designer, Design Technologist",
+            "tags": "Arduino, Bluetooth, HTML, CSS, Javascript",
+            "type": "School Work",
+            "num_images": "6"
+        }
+    ]
+}
